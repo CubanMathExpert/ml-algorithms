988481efdc483ef6af629a17ed043bce350e18fc
CubanMathExpert <rafaelglez98@hotmail.com>
2024-04-11 19:55:01 -0400

    64% accuracy naive-bayes /lowercase/punctuation/stopwords/max features = 500 instead of 10000

--- twitter.py
+++ twitter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import torch
+import string
 import transformers as ppb
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,21 +9,33 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 from nltk.stem import PorterStemmer
-nltk.download('punkt')
+from nltk.corpus import stopwords
+
+# nltk downloads
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
 dataFrame = pd.read_csv("source/offenseval-training-v1.tsv", delimiter="\t")
+stop_words = set(stopwords.words('english'))  # set of all the english stopwords
 
 # stem the file
 stemmer = PorterStemmer()
 def stem_text(text):
+    text = text.lower() # put text in lower case
+    text = text.translate(str.maketrans('', '', string.punctuation)) # remove punctuation
     tokens = nltk.word_tokenize(text)
-    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+
+    clean_tokens = [token for token in tokens if token not in  stop_words] # tokens without stop words
+    stemmed_tokens = [stemmer.stem(token) for token in clean_tokens] # stem the tokens without stop words
+
     return ' '.join(stemmed_tokens)
 
 dataFrame['stemmed_tweet'] = dataFrame['tweet'].apply(stem_text) # add column 'stemmed_tweet' to dataFrame
 
-vectorizer = TfidfVectorizer(max_features=10000)
+# tokenization
+vectorizer = TfidfVectorizer(max_features=500)
 X = vectorizer.fit_transform(dataFrame['stemmed_tweet'])
 X_dense = X.toarray()
 encoder = LabelEncoder()
@@ -31,27 +44,16 @@ y = encoder.fit_transform(dataFrame['subtask_a'])
 # train / test splits
 X_train, X_test, y_train, y_test = train_test_split(X_dense, y, test_size=0.7, random_state=0)
 
+# models
 model_nb = GaussianNB()
 
+# training and prediction
 y_pred = model_nb.fit(X_train, y_train).predict(X_test)
 
-print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
 
-
-#train_test_split fait le le 25-75 split test et train data??
-# ensuite je passe au model .predict(les donnes ?)
-#x is the input and y the target values.
-
-
-
-
-
-
-
-
-
-
-
+# check accuracy and full report 
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
 
 
 
