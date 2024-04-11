e184d29268917eec4f10940bd4cb3c4acc6eeda3
CubanMathExpert <rafaelglez98@hotmail.com>
2024-04-10 22:17:43 -0400

    first commit

---
+++ twitter.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import torch as pt
+import transformers as ppb
+from sklearn.feature_extraction.text import CountVectorizer
+import warnings
+warnings.filterwarnings('ignore')
+
+dataFrame = pd.read_csv("source/offenseval-training-v1.tsv", delimiter="\t")
+
+
+
+
+
+
+
+
+
