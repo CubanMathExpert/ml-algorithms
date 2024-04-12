991f79e0a5fc8dffe0a90fbc0fec3d75da02649d
CubanMathExpert <rafaelglez98@hotmail.com>
2024-04-12 00:35:00 -0400

    all classifiers done gotta tweak the values

--- twitter.py
+++ twitter.py
@@ -4,10 +4,15 @@ import torch
 import string
 import transformers as ppb
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import GaussianNB
+
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 from nltk.stem import PorterStemmer
@@ -17,7 +22,7 @@ from nltk.corpus import stopwords
 #nltk.download('punkt')
 #nltk.download('stopwords')
 
-dataFrame = pd.read_csv("source/offenseval-training-v1.tsv", delimiter="\t")
+dataFrame = pd.read_csv("source/offenseval-training-v1.tsv", delimiter="\t") # read tsv file with pandas
 stop_words = set(stopwords.words('english'))  # set of all the english stopwords
 
 # stem the file
@@ -35,7 +40,7 @@ def stem_text(text):
 dataFrame['stemmed_tweet'] = dataFrame['tweet'].apply(stem_text) # add column 'stemmed_tweet' to dataFrame
 
 # tokenization
-vectorizer = TfidfVectorizer(max_features=500)
+vectorizer = TfidfVectorizer(max_features=550) 
 X = vectorizer.fit_transform(dataFrame['stemmed_tweet'])
 X_dense = X.toarray()
 encoder = LabelEncoder()
@@ -44,16 +49,40 @@ y = encoder.fit_transform(dataFrame['subtask_a'])
 # train / test splits
 X_train, X_test, y_train, y_test = train_test_split(X_dense, y, test_size=0.7, random_state=0)
 
+
 # models
-model_nb = GaussianNB()
 
-# training and prediction
-y_pred = model_nb.fit(X_train, y_train).predict(X_test)
+# # naive bayes
+# model_nb = MultinomialNB()
+# y_pred_nb = model_nb.fit(X_train, y_train).predict(X_test)
+# print("Accuracy for Multinomial Naive Bayes: ", accuracy_score(y_test, y_pred_nb))
+
+# # decision tree
+# model_dt = DecisionTreeClassifier()
+# y_pred_dt = model_dt.fit(X_train, y_train).predict(X_test)
+# print("Accuracy for Decision Tree: ", accuracy_score(y_test, y_pred_dt))
+
+# # random forests
+# model_rf = RandomForestClassifier(n_estimators=10)
+# y_pred_rf = model_rf.fit(X_train, y_train).predict(X_test)
+# print("Accuracy for Random Forests: ", accuracy_score(y_test, y_pred_rf))
+
+# # SVM
+# model_svm_lin = SVC(kernel='linear')
+# model_svm_rbf = SVC(kernel='rbf')
+# y_pred_svm_lin = model_svm_lin.fit(X_train, y_train).predict(X_test)
+# y_pred_svm_rbf = model_svm_rbf.fit(X_train, y_train).predict(X_test)
+# print("Accuracy for SVM linear: ", accuracy_score(y_test, y_pred_svm_lin))
+# print("Accuracy for SVM rbf: ", accuracy_score(y_test, y_pred_svm_rbf))
+
+# neural network
+model_mlp = MLPClassifier(activation='logistic', hidden_layer_sizes=(100, 50), max_iter=800, momentum=0.9, learning_rate_init=0.001, batch_size=300, random_state=0)
+y_pred_mlp = model_mlp.fit(X_train, y_train).predict(X_test)
+print("Accuracy for Multi Layer Perceptron: ", accuracy_score(y_test, y_pred_mlp))
 
 
-# check accuracy and full report 
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Classification Report:\n", classification_report(y_test, y_pred))
+
+##print("Classification Report:\n", classification_report(y_test, y_pred))
 
 
 
