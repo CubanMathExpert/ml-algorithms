5e7719a1abd6130a7171e109756a55e87e6ecf00
CubanMathExpert <rafaelglez98@hotmail.com>
2024-04-13 01:37:33 -0400

    bert steps complete not doable on my macbook gotta do it with nvidia GPU

--- twitterBert.py
+++ twitterBert.py
@@ -18,10 +18,32 @@ dataFrame = pd.read_csv("source/offenseval-training-v1.tsv", delimiter='\t')
 
 dataFrame = dataFrame[['tweet', 'subtask_a']]
 
+model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = model_class.from_pretrained(pretrained_weights)
 
+# tokenization
+tokenized = dataFrame['tweet'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
 
+# padding
+max_len = 0
+for i in tokenized.values:
+    if len(i) > max_len:
+        max_len = len(i)
+padded = np.array([i + [0]*(max_len-len(i)) for i in tokenized.values])
 
+# masking
+attention_mask = np.where(padded != 0, 1, 0)
 
+input_ids = torch.tensor(padded)
+attention_mask = torch.tensor(attention_mask)
 
+result = []
+batch_size = 40
+
+with torch.no_grad():
+    for i in tqdm(range(0, input_ids.shape[0], batch_size)):
+        last_hiddden_states_for_batch = model(input_ids[i:i + batch_size], attention_mask=attention_mask[i:i + batch_size])
+        result.append(last_hiddden_states_for_batch)
 
 
